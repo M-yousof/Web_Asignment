87a9b21e18a7df8ab2471259660f561b1e27227d
A-Milad <masihullah047@gmail.com>
2021-03-28 12:40:22 -0700

    mohammad yousof homework

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer  class="navbar navbar-inverse navbar-fixed-bottom">
+				<div class="container">
+				<p class="navbar-text pull-left">All Right reserved With Me</p>
+	
+				</div>
+			</footer>
